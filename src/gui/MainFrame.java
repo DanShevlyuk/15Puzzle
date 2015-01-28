08971ca9547a7c0676ee495bf9ce9ae8465f68c9
reqwy <aapanin13@gmail.ru>
2015-01-28 23:14:09 +0300

    MainFrame

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,6 +1,9 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 /**
  * Main Frame for 15 Puzzle
@@ -10,20 +13,26 @@ public class MainFrame extends JFrame {
     private FRPuzzlePanel puzzlePanel;
 
     public MainFrame() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         initComponents();
-        initFrame();
+        Toolkit tk;
+        tk = Toolkit.getDefaultToolkit();
+        Dimension screen = tk.getScreenSize();
+        setLocation((int) screen.getWidth() / 4, (int) screen.getHeight() / 4);
+        pack();
+        setVisible(true);
+        puzzlePanel.makeCellViewsResizable();
+
     }
 
     private void initComponents() {
         //TODO: add listeners for puzzle panel
         //TODO: add  puzzlePanel to MainFrame
+        puzzlePanel = new FRPuzzlePanel();
+        add(puzzlePanel);
     }
 
-    private void initFrame() {
-        //init some
 
-        updateFrame();
-    }
 
     private void updateFrame() {
         //stuff
@@ -33,12 +42,18 @@ public class MainFrame extends JFrame {
     }
 
     private void jPanelPuzzleMouseClicked(java.awt.event.MouseEvent evt) {
+        System.out.println("sexyAreYou?");
         if (evt.getClickCount() == 0) {
             return;
         }
         puzzlePanel.cellClicked(evt);
 
         updateFrame();
+    }
+
+    public static void main(String[] args) {
+        new MainFrame();
+
     }
 
 }
