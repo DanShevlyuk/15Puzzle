5581c40f24a132e369c2431c3f3b013fa2d2b45c
reqwy <aapanin13@gmail.ru>
2015-01-28 23:11:27 +0300

    A Little modified

--- src/gui/FRPuzzlePanel.java
+++ src/gui/FRPuzzlePanel.java
@@ -1,28 +1,61 @@
 package gui;
 
+import model.FPCell;
 import model.FPPuzzle;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 /**
  * Игровое поле с клетками
  */
-public class FRPuzzlePanel extends JFrame {
-    FPPuzzle puzzle;
+public class FRPuzzlePanel extends JPanel {
+    private FPPuzzle puzzle;
+    private FPCellView cellViews[];
     //TODO: добавить размеры пазла и размер одной клетки и использовать их в cellClicked
 
     public FRPuzzlePanel() {
+        setLayout(new GridLayout(4, 4));
         initComponents();
-        initPanel();
+
     }
 
-    private void initPanel() {
 
+    @Override
+    public Dimension getPreferredSize(){
+        return new Dimension((int)(4 * cellViews[0].getPreferredSize().getWidth()),
+                (int)(4 * cellViews[0].getPreferredSize().getHeight()));
     }
 
     public void initComponents() {
+        puzzle = new FPPuzzle();
+        cellViews = new FPCellView[puzzle.getSize()];
+        for (int i = 0; i < cellViews.length; i++) {
+            cellViews[i] = new FPCellView(puzzle.get(i));
+            add(cellViews[i]);
+        }
 
     }
 
+    public FPPuzzle getPuzzle() {
+        return puzzle;
+    }
+
+    public void makeCellViewsResizable() {
+        for (int i = 0; i < cellViews.length; i++) {
+            addComponentListener(cellViews[i]);
+        }
+    }
+
+    public void makeViewsReactAtClicks(){
+        for (int i = 0; i < cellViews.length; i++) {
+            addMouseListener(cellViews[i]);
+        }
+    }
 
     public void cellClicked(java.awt.event.MouseEvent evt) {
         int row = 0;
@@ -39,4 +72,6 @@ public class FRPuzzlePanel extends JFrame {
         int index = 0;
         puzzle.moveMePlease(index);
     }
+
+
 }
