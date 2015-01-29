a3c4882e1eb07b61b534f4b7bb8c2749cb76555c
reqwy <aapanin13@gmail.ru>
2015-01-29 09:23:43 +0300

    added position to a cell

--- src/model/FPCell.java
+++ src/model/FPCell.java
@@ -1,11 +1,16 @@
 package model;
 
-public class FPCell {
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class FPCell  {
     private int value;
     private boolean isEmpty;
+    private int position;
 
-    public FPCell(int value) {
+    public FPCell(int value, int pos) {
         this.value = value;
+        position = pos;
         isEmpty = (value == -1);
     }
 
@@ -21,7 +26,14 @@ public class FPCell {
         return isEmpty;
     }
 
+    public int getPosition() {
+        return position;
+    }
+
     public void setEmpty(boolean isEmpty) {
         this.isEmpty = isEmpty;
     }
+
+    // Mouse Events occurred in View
+
 }
