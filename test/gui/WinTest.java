d1227147ca0f4451e55492afe5bbe87be7ade3ef
reqwy <aapanin13@gmail.ru>
2015-01-29 20:52:21 +0300

    Added test for congratulations

---
+++ test/gui/WinTest.java
@@ -0,0 +1,18 @@
+package gui;
+
+import model.FPCell;
+import model.FPPuzzle;
+import org.junit.Test;
+
+/**
+ * Created by reqwy on 29.01.2015.
+ */
+public class WinTest {
+
+
+    @Test
+    public void testTheWin() {
+        new CheatedMainFrame();
+        while (true){}
+    }
+}
